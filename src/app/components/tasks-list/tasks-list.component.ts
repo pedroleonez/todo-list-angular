893629d387683279e42fe627ca6b91e586b90500
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-07 21:45:36 -0300

    feat: create tasks-list component

---
+++ src/app/components/tasks-list/tasks-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-list',
+  imports: [],
+  templateUrl: './tasks-list.component.html'
+})
+export class TasksListComponent {
+
+}
