f6e6e9cc1ced6397b35cc72e5db2a0ed0f64d62a
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-09 14:26:10 -0300

    feat: initial impl tasks-list

--- src/app/components/tasks-list/tasks-list.component.ts
+++ src/app/components/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../interface/task';
 
@@ -9,6 +9,21 @@ import { Task } from '../../interface/task';
   })
 export default class TasksListComponent {
   @Input() tasks: Task[] = [];
+  @Output() add = new EventEmitter(false);
+  @Output() edit = new EventEmitter(false);
+  @Output() delete = new EventEmitter(false);
 
   readonly displayedColumns = ['id', 'title', 'description', 'completed', 'priority', 'actions'];
+
+  onAdd() {
+    this.add.emit(true);
+  }
+
+  onEdit(task: Task) {
+    this.edit.emit(task);
+  }
+
+  onDelete(task: Task) {
+    this.delete.emit(task);
+  }
 }
