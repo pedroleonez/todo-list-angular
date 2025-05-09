f36870b610bd07cc1a48962fc490cfc7aa95b9f7
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-09 14:25:24 -0300

    feat: initial impl routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import TasksListComponent from './components/tasks-list/tasks-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: TasksListComponent
+  },
+  {
+    path: 'tasks-list',
+    component: TasksListComponent
+  },
+  {
+    path: 'task-form',
+    component: TaskFormComponent
   }
 ];
