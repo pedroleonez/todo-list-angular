fe3304af70516398544794995cb742c353eed515
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-08 22:18:47 -0300

    feat: start impl tasks-list ts

--- src/app/components/tasks-list/tasks-list.component.ts
+++ src/app/components/tasks-list/tasks-list.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../interface/task';
 
 @Component({
   selector: 'app-tasks-list',
-  imports: [],
-  templateUrl: './tasks-list.component.html'
-})
-export class TasksListComponent {
+  imports: [CommonModule],
+  templateUrl: './tasks-list.component.html',
+  })
+export default class TasksListComponent {
+  @Input() tasks: Task[] = [];
 
+  readonly displayedColumns = ['id', 'title', 'description', 'completed', 'priority', 'actions'];
 }
