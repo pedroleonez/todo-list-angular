dda2c2741d6df0cd1f6b905a6390dd1541131849
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-08 22:18:09 -0300

    feat: add default route to test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import TasksListComponent from './components/tasks-list/tasks-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TasksListComponent
+  }
+];
